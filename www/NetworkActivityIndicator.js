4233505db17024356bf39f98be16a51e1eeb20a5
Ricardo <ricardocamara@MacBook-Pro-de-Ricardo.local>
2014-10-19 15:46:12 +0200

    Fix major issue

--- www/NetworkActivityIndicator.js
+++ www/NetworkActivityIndicator.js
@@ -1,18 +1,16 @@
-cordova.define("es.dev-coders.network-activity-indicator", function(require, exports, module) { 
-	var argscheck = require('cordova/argscheck'),
-    	utils = require('cordova/utils'),
-    	exec = require('cordova/exec');
+var argscheck = require('cordova/argscheck'),
+	utils = require('cordova/utils'),
+	exec = require('cordova/exec');
 
-    var NetworkActivityIndicator = function() {
-	};
+var NetworkActivityIndicator = function() {
+};
 
-	NetworkActivityIndicator.start = function() {
-		exec(null, null, "NetworkActivityIndicator", "start", []);
-	};
+NetworkActivityIndicator.start = function() {
+	exec(null, null, "NetworkActivityIndicator", "start", []);
+};
 
-	NetworkActivityIndicator.stop = function() {
-		exec(null, null, "NetworkActivityIndicator", "stop", []);
-	};
+NetworkActivityIndicator.stop = function() {
+	exec(null, null, "NetworkActivityIndicator", "stop", []);
+};
 
-	module.exports = NetworkActivityIndicator;
-});
+module.exports = NetworkActivityIndicator;
