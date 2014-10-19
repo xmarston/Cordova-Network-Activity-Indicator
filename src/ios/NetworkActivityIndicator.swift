cd5bbbf0d620281bc97a33a0c1ce2f3dc15d72ca
Ricardo <ricardocamara@MacBook-Pro-de-Ricardo.local>
2014-10-19 19:00:05 +0200

    Remove unnecessary stuff

--- src/ios/NetworkActivityIndicator.swift
+++ src/ios/NetworkActivityIndicator.swift
@@ -10,15 +10,6 @@ import Foundation
 
 
 @objc(NetworkActivityIndicator) class NetworkActivityIndicator : CDVPlugin {
-    /*func echo(command: CDVInvokedUrlCommand) {
-        var message = command.arguments[0] as String
-        
-        message = message.uppercaseString // Prove the plugin is actually doing something
-        
-        var pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: message)
-        commandDelegate.sendPluginResult(pluginResult, callbackId:command.callbackId)
-    }*/
-    
     func start(command: CDVInvokedUrlCommand) {
         UIApplication.sharedApplication().networkActivityIndicatorVisible = true
     }
