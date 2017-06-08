e5583990b0f0efd07e4db0bc395940d7d1d38848
Nan Wang <nan@nans-mbp.lan>
2017-06-08 20:53:18 +0800

    Network Status Indicator introduced

--- src/ios/NetworkActivityIndicator.swift
+++ src/ios/NetworkActivityIndicator.swift
@@ -8,11 +8,33 @@ import Foundation
 
 
 @objc(NetworkActivityIndicator) class NetworkActivityIndicator : CDVPlugin {
+    
+var networkCount = 0
+    
     func start(command: CDVInvokedUrlCommand) {
         UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        networkCount += 1
     }
     
     func stop(command: CDVInvokedUrlCommand) {
         UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+        networkCount -= 2
     }
+    
+    func statusIndicator(networkCountNum:Int) {
+        
+        if networkCount == 0 {
+            print("No Activity")
+        }
+        
+        if networkCount == 1 {
+            print("Network is connected")
+        }
+        
+        if networkCount == -1 {
+            print("NetworkCount is disconnected")
+        }
+    }
+    
+    
 }
