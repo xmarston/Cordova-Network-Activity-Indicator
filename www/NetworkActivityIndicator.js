e58d8696189e75feb17260bb3d813b643a97d30a
Jake Jarrett <jake.jarrett@pointsbet.com>
2019-02-04 16:23:06 +1100

    Make the name automatically derived from the class name

--- www/NetworkActivityIndicator.js
+++ www/NetworkActivityIndicator.js
@@ -1,14 +1,4 @@
-var  exec = require('cordova/exec');
-
-const pluginName = 'NetworkActivityIndicator';
-
-const callbackExec = (method, params) => {
-	return new Promise((resolve, reject) => {
-        const cb = (err, data) => err ? reject(new Error(err)) : resolve(data);
-        exec(cb.bind(null, undefined), cb, pluginName, method, params);
-	})
-};
-
+const exec = require('cordova/exec');
 
 class NetworkActivityIndicator {
 	start () {
@@ -19,5 +9,14 @@ class NetworkActivityIndicator {
 		return callbackExec("stop", []);
 	}
 }
+
+const pluginName = NetworkActivityIndicator.name;
+
+const callbackExec = (method, params) => {
+	return new Promise((resolve, reject) => {
+        const cb = (err, data) => err ? reject(new Error(err)) : resolve(data);
+        exec(cb.bind(null, undefined), cb, pluginName, method, params);
+	})
+};
 
 module.exports = new NetworkActivityIndicator();
